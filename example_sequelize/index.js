47c6bc40c617435c480cb1dfe4d56d5770b3d6d4
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-18 14:56:45 +0700

    Example with Sequelize

---
+++ example_sequelize/index.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize")
+
+const sequelize = new Sequelize(
+  "socialmedia", // Database Name
+  "haidar", // Database Username
+  "Notpassword!0", // Database Password
+  {
+    host: "localhost", // Database host
+    dialect: "mysql" // Database type
+  }
+)
+
+const User = sequelize.define("user", {
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  name: Sequelize.STRING,
+  username: Sequelize.STRING
+})
+
+const newUser = {
+  username: "mhaidarhanif",
+  email: "mhaidarhanif@gmail.com",
+  password: "helloworld",
+  name: "M Haidar Hanif"
+}
+
+User.sync({ force: true }).then(() => {
+  return User.create(newUser).then(data => {
+    console.log(data.user)
+  })
+})
