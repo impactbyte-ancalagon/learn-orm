742d87931f56685fc552dd115a9f9456a80a5b43
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-18 14:18:34 +0700

    Use mysql2 to access MySQL

--- example_mysql/index.js
+++ example_mysql/index.js
@@ -1,13 +1,22 @@
-const mysql = require("mysql2")
+const mysql = require("mysql")
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "haidar",
-  password: "helloworld",
+  password: "Notpassword!0",
   database: "todo_app"
 })
 
-connection.query("SELECT * FROM users", function(err, results, fields) {
-  console.log(results) // results contains rows returned by server
-  console.log(fields) // fields contains extra meta data about results, if available
+connection.query("SELECT * FROM `users`", function(err, results, fields) {
+  results.forEach(item => {
+    console.log(item)
+  })
 })
+
+connection.query("SELECT * FROM `todos`", function(err, results, fields) {
+  results.forEach(item => {
+    console.log(item)
+  })
+})
+
+// ALTER USER 'haidar'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Notpassword!0';
